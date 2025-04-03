498e93bb38a0090caaa83cfb7278be1d749440af
hassaammgl <h455a4mmehtab@gmail.com>
2025-04-04 04:30:11 +0500

    refactor menu structure and animations; remove unused dependencies

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,44 @@
 // styles
 import '../styles/style.scss'
 
+
 // scripts
 import { imgs } from "./imgs"
+import gsap from "gsap";
 import { menuButton } from './menu'
 
+function valueSetters() {
+    gsap.set("#menu", { clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)" })
+    gsap.set("#link .parent .child", { y: "100%", opacity: 0 })
+    // gsap.set("#home .row img", { opacity: 0 })
+}
+
+function revealToSpan() {
+    const h1Elems = document.getElementsByClassName("reveal");
+
+    for (let i = 0; i < h1Elems.length; i++) {
+        const e = h1Elems[i];
+
+        const orignalContent = e.innerHTML;
+        console.log(orignalContent);
+
+        e.innerHTML = "";
+
+        const spanP = document.createElement("span");
+        spanP.classList.add("parent")
+        const spanC = document.createElement("span");
+        spanC.classList.add("child")
+
+        spanC.innerHTML = orignalContent;
+
+        spanP.appendChild(spanC);
+        e.appendChild(spanP);
+    }
+
+}
+
+revealToSpan()
+valueSetters()
 menuButton()
 console.log(imgs);
 
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,10 @@
+import gsap from "gsap";
+
 export const menuButton = () => {
+    const tl = gsap.timeline();
     const menuBtn = document.getElementById("menubtn");
     const menuIcon = document.getElementById("menuicon");
+    // tl.to("#menu", { clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)" })
 
     menuBtn.addEventListener('click', () => {
         // Get the current source of the image
@@ -13,9 +17,25 @@ export const menuButton = () => {
         if (filename === "menu.svg") {
             menuIcon.src = "/icons/close.svg";
             menuIcon.alt = "close";
+            tl.to("#menu", {
+                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+                duration: 0.5,
+                ease: "power2.inOut"
+            })
         } else {
             menuIcon.src = "/icons/menu.svg";
             menuIcon.alt = "menu";
+            tl.reverse();
         }
     });
+
+    tl.to("#link .parent .child", {
+        y: "0%",
+        opacity: 1,
+        duration: 0.5,
+        stagger: 0.1,
+        delay: 0.5,
+        ease: "power2.inOut"
+    }, "<")
+
 }
